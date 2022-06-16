6d84bdf8134ffdb9043737fb51913d8de03a621d
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2022-06-16 10:10:54 +0530

    add fields to request and response
    Signed-off-by: ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>

--- src/main/java/com/example/doctorappointmentbooking/dto/AppointmentBookingRequest.java
+++ src/main/java/com/example/doctorappointmentbooking/dto/AppointmentBookingRequest.java
@@ -1,4 +1,14 @@
 package com.example.doctorappointmentbooking.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class AppointmentBookingRequest {
+
+    private Date fromDate;
+    private Date toDate;
 }
