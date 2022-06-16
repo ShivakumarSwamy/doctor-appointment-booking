815422c334fefb23bf186e08b5544d72fff6c792
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2022-06-16 10:10:13 +0530

    add annotations
    Signed-off-by: ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>

--- src/main/java/com/example/doctorappointmentbooking/jpa/AppointmentBookingRepository.java
+++ src/main/java/com/example/doctorappointmentbooking/jpa/AppointmentBookingRepository.java
@@ -2,8 +2,9 @@ package com.example.doctorappointmentbooking.jpa;
 
 import com.example.doctorappointmentbooking.domain.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface AppointmentBookingRepository extends JpaRepository<Appointment, String> {
 
 }
--- src/main/java/com/example/doctorappointmentbooking/service/AppointmentBookingServiceImpl.java
+++ src/main/java/com/example/doctorappointmentbooking/service/AppointmentBookingServiceImpl.java
@@ -2,8 +2,9 @@ package com.example.doctorappointmentbooking.service;
 
 import com.example.doctorappointmentbooking.domain.Appointment;
 import com.example.doctorappointmentbooking.dto.AppointmentBookingRequest;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class AppointmentBookingServiceImpl implements AppointmentBookingService {
 
     @Override
