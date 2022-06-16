fd7fd69f3a2abbe12d35ec0d3d6268b48bd1ad94
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2022-06-16 10:15:20 +0530

    add getAllAppointments
    Signed-off-by: ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>

--- src/main/java/com/example/doctorappointmentbooking/service/AppointmentBookingService.java
+++ src/main/java/com/example/doctorappointmentbooking/service/AppointmentBookingService.java
@@ -3,8 +3,11 @@ package com.example.doctorappointmentbooking.service;
 import com.example.doctorappointmentbooking.domain.Appointment;
 import com.example.doctorappointmentbooking.dto.AppointmentBookingRequest;
 
+import java.util.Collection;
+
 public interface AppointmentBookingService {
 
     Appointment createAppointment(AppointmentBookingRequest appointmentBookingRequest);
 
+    Collection<Appointment> getAllAppointments();
 }
--- src/main/java/com/example/doctorappointmentbooking/service/AppointmentBookingServiceImpl.java
+++ src/main/java/com/example/doctorappointmentbooking/service/AppointmentBookingServiceImpl.java
@@ -4,11 +4,18 @@ import com.example.doctorappointmentbooking.domain.Appointment;
 import com.example.doctorappointmentbooking.dto.AppointmentBookingRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 public class AppointmentBookingServiceImpl implements AppointmentBookingService {
 
     @Override
     public Appointment createAppointment(AppointmentBookingRequest appointmentBookingRequest) {
+        return null;
+    }
+
+    @Override
+    public Collection<Appointment> getAllAppointments() {
         return null;
     }
 }
