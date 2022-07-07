c388493090d6e6e1a89dd9fef392004128b37db5
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2022-07-07 15:58:30 +0530

    add create appointment skeleton code
    Signed-off-by: ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>

--- src/main/java/com/example/doctorappointmentbooking/controller/AppointmentBookingController.java
+++ src/main/java/com/example/doctorappointmentbooking/controller/AppointmentBookingController.java
@@ -1,8 +1,21 @@
 package com.example.doctorappointmentbooking.controller;
 
+import com.example.doctorappointmentbooking.domain.Appointment;
+import com.example.doctorappointmentbooking.dto.AppointmentBookingRequest;
+import com.example.doctorappointmentbooking.service.AppointmentBookingService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppointmentBookingController {
 
+    private final AppointmentBookingService appointmentBookingService;
+
+    public AppointmentBookingController(AppointmentBookingService appointmentBookingService) {
+        this.appointmentBookingService = appointmentBookingService;
+    }
+
+    public Appointment createAppointment(@RequestBody AppointmentBookingRequest appointmentBookingRequest) {
+        return appointmentBookingService.createAppointment(appointmentBookingRequest);
+    }
 }
